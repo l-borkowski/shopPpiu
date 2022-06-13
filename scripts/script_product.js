6772b1de64a1f23b597d1931982d7bc60ca480d0
Yosaire <96118628+Yosaire@users.noreply.github.com>
2022-06-13 17:11:36 +0200

    Naprawiono Slider i dodano odnoniki do stron

--- scripts/script_product.js
+++ scripts/script_product.js
@@ -137,6 +137,8 @@ const container_slider_boxes = document.querySelectorAll(".main_offers_container
 const prevBtn = document.querySelector("#prevBtn");
 const nextBtn = document.querySelector("#nextBtn");
 
+const iframe = document.querySelector("#iframe");
+
 let counter = 1;
 
 container_slider.style.transform = "translateX(-275px)";
@@ -149,7 +151,6 @@ nextBtn.addEventListener("click", function()
     }
     container_slider.style.transition = "transform 0.4s ease-in-out";
     counter++;
-    console.log(counter);
     container_slider.style.transform = "translateX("+(-279*counter) + "px)";
 });
 
@@ -180,21 +181,277 @@ container_slider.addEventListener("transitionend",  function()
     }
 });
 
-console.log(container_slider_boxes.length);
+//for(var i=0; i<container_slider_boxes.length;i++)
+//{}
+    var self0 = container_slider_boxes[0];
+    var self1 = container_slider_boxes[1];
+    var self2 = container_slider_boxes[2];
+    var self3 = container_slider_boxes[3];
+    var self4 = container_slider_boxes[4];
+    var self5 = container_slider_boxes[5];
+    var self6 = container_slider_boxes[6];
+    var self7 = container_slider_boxes[7];
+    var self8 = container_slider_boxes[8];
+    var self9 = container_slider_boxes[9];
+    var self10 = container_slider_boxes[10];
 
-    container_slider_boxes[1].addEventListener("mouseenter", function()
+    self0.addEventListener("click", function()
     {
-        container_slider_boxes[1].style.borderBottom = "5px solid #667DB9";
+        //var img = document.createElement("img");
+        var img = "<img src={{product.pictures[0]}} width='100%'  height='500px'/>";
+        main_sliderbox_image.innerHTML = img;
+        self0.style.marginBottom = "20px";
+        self0.style.borderBottom = "5px solid #667DB9";
+        self1.style.borderBottom = "none";
+        self2.style.borderBottom = "none";
+        self3.style.borderBottom = "none";
+        self4.style.borderBottom = "none";
+        self5.style.borderBottom = "none";
+        self6.style.borderBottom = "none";
+        self7.style.borderBottom = "none";
+        self8.style.borderBottom = "none";
+        self9.style.borderBottom = "none";
+        self10.style.borderBottom = "none";
+        console.log(img);
+    
+        //iframe.style.display = "none";
+       // main_sliderbox_image.appendChild(img);
     });
 
-for(i=0;i<container_slider_boxes.length;i++)
+    self1.addEventListener("click", function()
+    {
+        //var img = document.createElement("img");
+        var img = "<img src={{product.pictures[1]}} width='100%'  height='500px'/>";
+        main_sliderbox_image.innerHTML = img;
+        self1.style.marginBottom = "20px";
+        self1.style.borderBottom = "5px solid #667DB9";
+        self0.style.borderBottom = "none";
+        self2.style.borderBottom = "none";
+        self3.style.borderBottom = "none";
+        self4.style.borderBottom = "none";
+        self5.style.borderBottom = "none";
+        self6.style.borderBottom = "none";
+        self7.style.borderBottom = "none";
+        self8.style.borderBottom = "none";
+        self9.style.borderBottom = "none";
+        self10.style.borderBottom = "none";
+        console.log(img);
+        //iframe.style.display = "none";
+       // main_sliderbox_image.appendChild(img);
+    });
+
+    self2.addEventListener("click", function()
+    {
+        //var img = document.createElement("img");
+        var img = "<img src={{product.pictures[2]}} width='100%'  height='500px'/>";
+        main_sliderbox_image.innerHTML = img;
+        self2.style.marginBottom = "20px";
+        self2.style.borderBottom = "5px solid #667DB9";
+        self1.style.borderBottom = "none";
+        self0.style.borderBottom = "none";
+        self3.style.borderBottom = "none";
+        self4.style.borderBottom = "none";
+        self5.style.borderBottom = "none";
+        self6.style.borderBottom = "none";
+        self7.style.borderBottom = "none";
+        self8.style.borderBottom = "none";
+        self9.style.borderBottom = "none";
+        self10.style.borderBottom = "none";
+        console.log(img);
+        //iframe.style.display = "none";
+       // main_sliderbox_image.appendChild(img);
+    });
+
+    self3.addEventListener("click", function()
+    {
+        //var img = document.createElement("img");
+        var img = "<img src={{product.pictures[3]}} width='100%'  height='500px'/>";
+        main_sliderbox_image.innerHTML = img;
+        self3.style.marginBottom = "20px";
+        self3.style.borderBottom = "5px solid #667DB9";
+        self3.style.borderBottom = "5px solid #667DB9";
+        self1.style.borderBottom = "none";
+        self2.style.borderBottom = "none";
+        self0.style.borderBottom = "none";
+        self4.style.borderBottom = "none";
+        self5.style.borderBottom = "none";
+        self6.style.borderBottom = "none";
+        self7.style.borderBottom = "none";
+        self8.style.borderBottom = "none";
+        self9.style.borderBottom = "none";
+        self10.style.borderBottom = "none";
+        console.log(img);
+        //iframe.style.display = "none";
+       // main_sliderbox_image.appendChild(img);
+    });
+
+    self4.addEventListener("click", function()
+    {
+        //var img = document.createElement("img");
+        var img = "<img src={{product.pictures[4]}} width='100%'  height='500px'/>";
+        main_sliderbox_image.innerHTML = img;
+        self4.style.marginBottom = "20px";
+        self4.style.borderBottom = "5px solid #667DB9";
+        self4.style.borderBottom = "5px solid #667DB9";
+        self1.style.borderBottom = "none";
+        self2.style.borderBottom = "none";
+        self3.style.borderBottom = "none";
+        self0.style.borderBottom = "none";
+        self5.style.borderBottom = "none";
+        self6.style.borderBottom = "none";
+        self7.style.borderBottom = "none";
+        self8.style.borderBottom = "none";
+        self9.style.borderBottom = "none";
+        self10.style.borderBottom = "none";
+        console.log(img);
+        //iframe.style.display = "none";
+       // main_sliderbox_image.appendChild(img);
+    });
+
+    self5.addEventListener("click", function()
+    {
+        //var img = document.createElement("img");
+        var img = "<img src={{product.pictures[5]}} width='100%'  height='500px'/>";
+        main_sliderbox_image.innerHTML = img;
+        self5.style.marginBottom = "20px";
+        self5.style.borderBottom = "5px solid #667DB9";
+        self1.style.borderBottom = "none";
+        self2.style.borderBottom = "none";
+        self3.style.borderBottom = "none";
+        self4.style.borderBottom = "none";
+        self0.style.borderBottom = "none";
+        self6.style.borderBottom = "none";
+        self7.style.borderBottom = "none";
+        self8.style.borderBottom = "none";
+        self9.style.borderBottom = "none";
+        self10.style.borderBottom = "none";
+        console.log(img);
+        //iframe.style.display = "none";
+       // main_sliderbox_image.appendChild(img);
+    });
+
+    self6.addEventListener("click", function()
+    {
+        //var img = document.createElement("img");
+        var img = "<img src={{product.pictures[6]}} width='100%'  height='500px'/>";
+        main_sliderbox_image.innerHTML = img;
+        self6.style.marginBottom = "20px";
+        self6.style.borderBottom = "5px solid #667DB9";
+        self1.style.borderBottom = "none";
+        self2.style.borderBottom = "none";
+        self3.style.borderBottom = "none";
+        self4.style.borderBottom = "none";
+        self5.style.borderBottom = "none";
+        self0.style.borderBottom = "none";
+        self7.style.borderBottom = "none";
+        self8.style.borderBottom = "none";
+        self9.style.borderBottom = "none";
+        self10.style.borderBottom = "none";
+        console.log(img);
+        //iframe.style.display = "none";
+       // main_sliderbox_image.appendChild(img);
+    });
+
+    self7.addEventListener("click", function()
+    {
+        //var img = document.createElement("img");
+        var img = "<img src={{product.pictures[7]}} width='100%'  height='500px'/>";
+        main_sliderbox_image.innerHTML = img;
+        self7.style.marginBottom = "20px";
+        self7.style.borderBottom = "5px solid #667DB9";
+        self1.style.borderBottom = "none";
+        self2.style.borderBottom = "none";
+        self3.style.borderBottom = "none";
+        self4.style.borderBottom = "none";
+        self5.style.borderBottom = "none";
+        self6.style.borderBottom = "none";
+        self0.style.borderBottom = "none";
+        self8.style.borderBottom = "none";
+        self9.style.borderBottom = "none";
+        self10.style.borderBottom = "none";
+        console.log(img);
+        //iframe.style.display = "none";
+       // main_sliderbox_image.appendChild(img);
+    });
+
+    self8.addEventListener("click", function()
+    {
+        //var img = document.createElement("img");
+        var img = "<img src={{product.pictures[8]}} width='100%'  height='500px'/>";
+        main_sliderbox_image.innerHTML = img;
+        self8.style.marginBottom = "20px";
+        self8.style.borderBottom = "5px solid #667DB9";
+        self1.style.borderBottom = "none";
+        self2.style.borderBottom = "none";
+        self3.style.borderBottom = "none";
+        self4.style.borderBottom = "none";
+        self5.style.borderBottom = "none";
+        self6.style.borderBottom = "none";
+        self7.style.borderBottom = "none";
+        self0.style.borderBottom = "none";
+        self9.style.borderBottom = "none";
+        self10.style.borderBottom = "none";
+        console.log(img);
+        //iframe.style.display = "none";
+       // main_sliderbox_image.appendChild(img);
+    });
+
+    self9.addEventListener("click", function()
+    {
+        //var img = document.createElement("img");
+        var img = "<img src={{product.pictures[9]}} width='100%'  height='500px'/>";
+        main_sliderbox_image.innerHTML = img;
+        self9.style.marginBottom = "20px";
+        self9.style.borderBottom = "5px solid #667DB9";
+        self1.style.borderBottom = "none";
+        self2.style.borderBottom = "none";
+        self3.style.borderBottom = "none";
+        self4.style.borderBottom = "none";
+        self5.style.borderBottom = "none";
+        self6.style.borderBottom = "none";
+        self7.style.borderBottom = "none";
+        self8.style.borderBottom = "none";
+        self0.style.borderBottom = "none";
+        self10.style.borderBottom = "none";
+        console.log(img);
+        //iframe.style.display = "none";
+       // main_sliderbox_image.appendChild(img);
+    });
+
+    self10.addEventListener("click", function()
+    {
+        //var img = document.createElement("img");
+        var img = "<img src={{product.pictures[10]}} width='100%'  height='500px'/>";
+        main_sliderbox_image.innerHTML = img;
+        self10.style.marginBottom = "20px";
+        self10.style.borderBottom = "5px solid #667DB9";
+        self1.style.borderBottom = "none";
+        self2.style.borderBottom = "none";
+        self3.style.borderBottom = "none";
+        self4.style.borderBottom = "none";
+        self5.style.borderBottom = "none";
+        self6.style.borderBottom = "none";
+        self7.style.borderBottom = "none";
+        self8.style.borderBottom = "none";
+        self9.style.borderBottom = "none";
+        self0.style.borderBottom = "none";
+        console.log(img);
+        //iframe.style.display = "none";
+       // main_sliderbox_image.appendChild(img);
+    });
+ 
+
+//przyciski
+const button_add = document.querySelector("#button_add");
+
+button_add.addEventListener("click", function()
 {
-    container_slider_boxes[i].addEventListener("mouseenter", function()
+    if(button_add.innerHTML == "PRODUKT W KOSZYKU")
     {
-        container_slider_boxes[i].style.borderBottom = "5px solid #667DB9";
-    });
-    container_slider_boxes[i].addEventListener("mouseover", function()
+        button_add.innerHTML = "DODAJ DO KOSZYKA";
+    }
+    else
     {
-        container_slider_boxes[i].style.borderBottom = "5px solid red";
-    });
-}
+        button_add.innerHTML = "PRODUKT W KOSZYKU";
+    }
+});
